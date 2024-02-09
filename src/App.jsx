4992f816adbf9700d385f205ddd7dd38d61006e4
Kapil Rathore <116137491+rathorekapil20@users.noreply.github.com>
2024-02-09 23:30:21 +0530

    login

--- src/App.jsx
+++ src/App.jsx
@@ -18,13 +18,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './component/Login/Login'
+import SignUp from './component/SignUp/SignUp'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -43,7 +45,12 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
+
+
+      <Login/>
+      <SignUp/>
+      
     </>
   )
 }
