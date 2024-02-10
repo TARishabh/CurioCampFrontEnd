b4f0d95222f457f3739a6558878098f031911b8e
Tanay <tanaypatil210192@acropolis.in>
2024-02-10 21:35:32 +0530

    new file:   index.html

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './component/Navbar'
+import Signin from './component/Authentication/Signin'
+import Card from './component/Card'
+import Modal from './component/Modal'
+import Hero from './component/Hero'
+import Signup from './component/Authentication/Signup'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+
+      <Hero />
+      {/* <Navbar /> */}
+      <Signup />
+
+      {/* <Card />
+      <Modal /> */}
+      {/* <Login/>
+      <SignUp/> */}
+      
+    </>
+  )
+}
+
+export default App;
