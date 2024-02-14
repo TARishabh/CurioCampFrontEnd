91c23b9f62982a26187154779334ae662e8c90ec
Kapil Rathore <116137491+rathorekapil20@users.noreply.github.com>
2024-02-15 01:34:38 +0530

    pay card remove

--- src/courseContent/PaymentCard.jsx
+++ src/courseContent/PaymentCard.jsx
@@ -109,32 +109,12 @@ const PaymentCard = ({price}) => {
       })
   } 
   return (
-    // <div>
+    <div>
         
-    //   <button type="button" className="btn btn-light fw-semibold py-3" onClick={razorPay}>Upgrad now</button>
-    // </div>
+      <button type="button" className="btn btn-light fw-semibold py-3" onClick={razorPay}>Upgrad now</button>
+    </div>
 
-    <Card className="mt-6 w-96">
-      <CardHeader color="blue-gray" className="relative h-56">
-        <img src={Course1}
-          alt="card-image"
-        />
-      </CardHeader>
-      <CardBody>
-        <Typography variant="h5" color="blue-gray" className="mb-2">
-          UI/UX Review Check
-        </Typography>
-        <Typography>
-          The place is close to Barceloneta Beach and bus stop just 2 min by
-          walk and near to &quot;Naviglio&quot; where you can enjoy the main
-          night life in Barcelona.
-        </Typography>
-      </CardBody>
-      <CardFooter className="pt-0">
-        <Button>Read More</Button>
-      </CardFooter>
-    </Card>
-
+    
   )
 }
 
